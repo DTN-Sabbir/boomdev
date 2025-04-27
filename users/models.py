ba8418a1fd910dee8ccd8a937d3fb1d9f3c2e538
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-04-27 16:36:20 +0600

    Add custom user model and manager with migration files

--- users/models.py
+++ users/models.py
@@ -1,17 +1,60 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
-# Create your models here.
+from users.user_manager.custom_user_manager import CustomUserManager
+
+
+# Custom User Model
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    class Role(models.TextChoices):
+    class Roles(models.TextChoices):
         ADMIN = "ADMIN", _("Admin")
-        MANAGER = "MANAGER", _("MANAGER")
-        USER = "USER", _("USER")
+        MANAGER = "MANAGER", _("Manager")
+        USER = "USER", _("User")
 
     email = models.EmailField(unique=True)
-    name = models.CharField(max_length=150)
-    role = models.CharField(choices=Role.choices, max_length=50)
-    is_active = models.BooleanField(default=False)
+    name = models.CharField(max_length=255)
+    role = models.CharField(max_length=10, choices=Roles.choices, default=Roles.USER)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):
+
+        return self.email
+
+    @property
+    def is_admin(self):
+        return self.role == self.Roles.ADMIN
+
+    @property
+    def is_manager(self):
+        return self.role == self.Roles.MANAGER
+
+    @property
+    def is_user(self):
+        return self.role == self.Roles.USER
+
+
+import uuid
+
+
+class PasswordResetCode(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="rest_codes"
+    )
+    code = models.UUIDField(default=uuid.uuid4, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+
+
+def is_expired(self):
+    return timezone.now() > self.expires_at
