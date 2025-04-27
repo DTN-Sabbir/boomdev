b884b51f9c140da13fa461b6d4f88bb8a3666771
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-04-27 16:29:00 +0600

    Add custom user model configuration and enable CORS in settings

--- backend/settings.py
+++ backend/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.CustomUser"
 CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
